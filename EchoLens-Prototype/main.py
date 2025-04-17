4b198cc96956de197df18c55ef09e3720a13617d
Mohamed Hamdy <141698325+Mohamed-Hamdey@users.noreply.github.com>
2025-04-17 20:22:46 +0200

    Delete EchoLens-Prototype directory

--- EchoLens-Prototype/main.py
+++
@@ -1,8 +0,0 @@
-from audio_listener import start_audio_listener
-
-def main():
-    print("🔊 Starting EchoLens Audio Listener...")
-    start_audio_listener()
-
-if __name__ == "__main__":
-    main()
